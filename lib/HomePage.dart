d33d1f46ffaa32408f96993bb94348de3c321cea
Ranaco <ranasatyamraj@gmail.com>
2021-09-22 12:49:54 +0530

    sed

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, prefer_const_constructors
+// ignore_for_file: file_names, prefer_const_constructors, must_be_immutable, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,26 +17,88 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Container(
-            height: 200,
-            decoration: BoxDecoration(
-                gradient: const LinearGradient(colors: [
-                  Colors.blue,
-                  Colors.red,
-                ]),
-                borderRadius: BorderRadius.vertical(
-                    bottom: Radius.elliptical(
-                        MediaQuery.of(context).size.width, 80))),
-          ),
-          SingleChildScrollView(
-            child: Card(
-              child: Column(
-                children: [
-                  ListTile(
-                    title: Text(widget.email!),
-                  )
-                ],
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(colors: [
+                      Colors.blue,
+                      Colors.red,
+                    ]),
+                    borderRadius: BorderRadius.vertical(
+                        bottom: Radius.elliptical(
+                            MediaQuery.of(context).size.width, 80))),
               ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(50, 130, 0, 0),
+                child: GestureDetector(
+                  child: Opacity(
+                    opacity: 0.6,
+                    child: Container(
+                        width: 300,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        child: Row(
+                          children: <Widget>[
+                            SizedBox(
+                              width: 15,
+                            ),
+                            SizedBox(height: 20, child: Icon(Icons.search)),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            SizedBox(
+                              width: 80,
+                              child: Text('Search'),
+                            )
+                          ],
+                        )),
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/logo.jpeg'))),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Name :  " + widget.email!,
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Password : ' + widget.password!,
+                        style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+              ],
             ),
           )
         ],
